3fe433b02b16ba866e27f51a2ccb53b61f2350e6
RoseMagura <magurarm@guilford.edu>
2020-10-17 20:45:15 -0400

    Feat: Working on Security Tests

--- src/test/java/com/example/demo/security/SecurityTest.java
+++ src/test/java/com/example/demo/security/SecurityTest.java
@@ -1,14 +1,96 @@
 package com.example.demo.security;
 
+import com.example.demo.model.requests.CreateUserRequest;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import javax.print.attribute.standard.Media;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 public class SecurityTest {
-    //TODO: Setup user
-    public void setUpUser(){
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JacksonTester<CreateUserRequest> json;
+
+    public void createUser(String username, String password, String confirmPassword) throws Exception {
+        CreateUserRequest request = new CreateUserRequest();
+        request.setUsername(username);
+        request.setPassword(password);
+        request.setConfirmPassword(confirmPassword);
+        this.mockMvc.perform(post("/api/user/create")
+                .content(json.write(request).getJson())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON_UTF8));
+    }
+    @Test
+    public void testCreateUserSuccess() throws Exception {
+        CreateUserRequest request = new CreateUserRequest();
+        request.setUsername("test");
+        request.setPassword("testPassword");
+        request.setConfirmPassword("testPassword");
+        this.mockMvc.perform(post("/api/user/create")
+            .content(json.write(request).getJson())
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1));
+    }
+
+    @Test
+    public void testCreateUserPasswordError() throws Exception {
+        CreateUserRequest request = new CreateUserRequest();
+        request.setUsername("test");
+        request.setPassword("test");
+        request.setConfirmPassword("testP");
+        this.mockMvc.perform(post("/api/user/create")
+                .content(json.write(request).getJson())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isBadRequest());
+    }
+    @Test
+    public void testLoginSuccess() throws Exception {
+        createUser("test", "testPassword", "testPassword");
+        JSONObject obj = new JSONObject();
+        obj.put("username", "test");
+        obj.put("password", "testPassword");
+        MvcResult mvcResult = this.mockMvc.perform(post("/login")
+                .content(String.valueOf(obj))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON_UTF8)).andReturn();
+        System.out.println(mvcResult.getResponse());
+        assertNotNull(mvcResult.getResponse().getHeaderValue("Authorization"));
+
+//        mvcResult.andExpect(status().isOk())
+//        .andExpect(MockMvcResultMatchers.jsonPath("$.Authorization").exists())
+
         
     }
-    //create user
-
-    //log in
-
     //TODO: tests
     //test that user profile gives 403 if not logged in
 
